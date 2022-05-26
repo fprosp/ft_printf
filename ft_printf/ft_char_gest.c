45a9edcc986287ae8930db489c025338a1fba8b5
Francesco Prosperi Mangili <99927384+fprosp@users.noreply.github.com>
2022-05-26 14:17:52 +0200

    Com1

---
+++ ft_printf/ft_char_gest.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_char_gest.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fprosper <fprosper@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 12:19:26 by fprosper          #+#    #+#             */
+/*   Updated: 2022/05/23 15:22:15 by fprosper         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_print_char(va_list arg)
+{
+	char	buffer;
+
+	buffer = va_arg(arg, int);
+	ft_putchar(buffer);
+	return (1);
+}
+
+int	ft_putstr(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+	{
+		ft_putstr("(null)");
+		return (6);
+	}
+	while (i < ft_strlen(s))
+	{
+		ft_putchar(*(s + i));
+		i++;
+	}
+	return (i);
+}
+
+int	ft_put_id(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n < 0)
+	{
+		if (n == -2147483648)
+		{
+			return (write(1, "-2147483648", 11));
+		}
+		n *= (-1);
+		return ((write(1, "-", 1)) + ft_get_conv(n, '0', 10));
+	}
+	else
+		return (ft_get_conv(n, '0', 10));
+}
